981ab60f8512a931190c5628b071ab1c6656258e
Zenen Jaimes <zenen@fortressiq.com>
2022-12-12 19:57:12 -0800

    chore(build): gradle ready?

--- src/test/java/th/co/geniustree/springdata/jpa/specification/DocumentSpecs.java
+++ src/test/java/th/co/geniustree/springdata/jpa/specification/DocumentSpecs.java
@@ -2,13 +2,12 @@ package th.co.geniustree.springdata.jpa.specification;
 
 import org.springframework.data.jpa.domain.Specification;
 import th.co.geniustree.springdata.jpa.domain.Document;
-import th.co.geniustree.springdata.jpa.domain.Document_;
 
 /**
  * Created by pramoth on 9/29/2016 AD.
  */
 public class DocumentSpecs {
     public static Specification<Document> idEq(Long id){
-        return (root, query, cb) -> cb.equal(root.get(Document_.id),id);
+        return (root, query, cb) -> cb.equal(root.get(Document.id),id);
     }
 }
